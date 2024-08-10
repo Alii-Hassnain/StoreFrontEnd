45422605fd027964e198f709bbd9b1072985682c
Hassnain <tohassnain@gmail.com>
2024-08-10 11:17:56 +0500

    improved navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,45 +2,94 @@ import React from "react";
 import { FaMoon } from "react-icons/fa";
 import { MdSunny } from "react-icons/md";
 import { BsCart3 } from "react-icons/bs";
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+import { FaBarsStaggered } from "react-icons/fa6";
 
 const Navbar = () => {
   return (
-    <nav className="bg-blue-100">
-      <div className="flex items-center p-3 justify-between align-element">
-        {/* start */}
-        <div>
-          <button className="bg-primary p-4 px-6 rounded-lg font-bold text-xl text-white">
-            A
-          </button>
+    <nav className="bg-base-200">
+      <div className="navbar align-element">
+        <div className="navbar-start">
+          <NavLink
+            to="/"
+            className="hidden lg:flex btn btn-primary text-3xl items-center"
+          >
+            Ali
+          </NavLink>
+          <div className="dropdown dropdown-bottom">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <FaBarsStaggered className="text-2xl"/>
+            </div>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+            >
+            <li>
+              <NavLink to="/">
+                <button>Home</button>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">
+                <button>About</button>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/products">
+                <button>Products</button>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/cart">
+                <button>Cart</button>
+              </NavLink>
+            </li>
+              
+              
+            </ul>
+          </div>
         </div>
+        {/* start */}
         {/* middle */}
-        <div className="flex flex-row gap-x-5">
-          <Link to="/">
-            <button className="btn btn-ghost">Home</button>
-          </Link>
-          <Link to="/about">
-            <button className="btn btn-ghost">About</button>
-          </Link>
-          <Link to="/products">
-            <button className="btn btn-ghost">Products</button>
-          </Link>
-          <Link to="/cart">
-            <button className="btn btn-ghost">Cart</button>
-          </Link>
+        <div className="navbar-center hidden lg:flex">
+
+          <ul className="menu menu-horizontal gap-3">
+            <li>
+              <NavLink to="/">
+                <button>Home</button>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">
+                <button>About</button>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/products">
+                <button>Products</button>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/cart">
+                <button>Cart</button>
+              </NavLink>
+            </li>
+          </ul>
           
         </div>
         {/* end */}
-        <div className="flex gap-x-3">
-          
+        <div className="navbar-end">
           <div className="btn btn-ghost text-xl">
             <FaMoon />
           </div>
-          <Link to='/cart'>
-            <div className="btn btn-ghost text-xl">
-              <BsCart3 />
-            </div>
-          </Link>
+
+          <NavLink to="/cart" className="btn btn-ghost btn-circle btn-md ml-4">
+                <div className="indicator">
+                  <BsCart3 className="text-2xl"/>
+                  <span className="badge badge-sm badge-primary indicator-item">8</span>
+                </div>
+          </NavLink>
+
         </div>
       </div>
     </nav>
