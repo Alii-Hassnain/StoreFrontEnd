029af32b0b6d21da04cc3fe4454d44c5ee36ffce
Hassnain <tohassnain@gmail.com>
2024-08-14 09:48:28 +0500

    final single product

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -4,7 +4,7 @@ import { useLoaderData } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { SubmitBtn } from "../components";
 
-export const loader = async () => {
+export const loader = async ({ params }) => {
   const URL = `/products/${19}`;
   const response = await CustomUri(URL);
   const singleProduct = response.data.data;
@@ -12,72 +12,87 @@ export const loader = async () => {
 };
 
 const SingleProduct = () => {
-  const [colored, setColor] = useState();
-  const [amount, setAmount] = useState();
+  const [amount, setAmount] = useState(1);
   const { singleProduct } = useLoaderData();
 
-  const { image, title, price, description, color, company } =
+  const { image, title, price, description, colors, company } =
     singleProduct.attributes;
+  const [productColor, setProductColor] = useState(colors[0]);
   console.log(singleProduct);
+
+  const handleAmount = (e)=>{
+    setAmount(e.target.value)
+  }
   return (
-    <div className="align-element py-44">
-
-    <div className="breadcrumbs text-sm">
-      <ul>
-        <li>
-          <Link to="/home">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/products">
-            Products
-          </Link>
-        </li>
-      </ul>
-    </div>
-    
-    <div className="grid lg:grid-cols-2 lg:gap-x-16 min-h-screen mt-6">
-      {/* left */}
-      <div>
-        <img
-          className="h-96 w-96 lg:w-full object-cover  rounded-md"
-          src={image}
-          alt={title}
-        />
+    <div className="mt-40">
+      <div className="breadcrumbs text-sm">
+        <ul>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
       </div>
-      {/* right */}
-      <div className="max-w-2xl flex flex-col gap-4">
-        <h1 className="text-3xl text-gray-700 font-bold">{title}</h1>
-        <h2>{company}</h2>
-        <h1>${price / 100}</h1>
-        <p>{description}</p>
-        <div className="flex flex-col gap-4">
-          <h1 className="">Colors</h1>
-          <div className="flex gap-3">
-            <button className="btn btn-circle btn-sm bg-blue-600"></button>
-            <button className="btn btn-circle btn-sm bg-green-400"></button>
-          </div>
 
-          <h1>Amount</h1>
-          <select className="select select-bordered w-full max-w-xs">
-            <option disabled selected>
-              1
-            </option>
-            <option>2</option>
-            <option>3</option>
-          </select>
-
-          <div className="w-1/4">
-          <SubmitBtn text="ADD TO BAG"/>
-
-          </div>
-          
+      <div className="grid lg:grid-cols-2 lg:gap-x-16 min-h-screen mt-6">
+        {/* left */}
+        <div>
+          <img
+            className="h-96 w-96 lg:w-full object-cover  rounded-md"
+            src={image}
+            alt={title}
+          />
         </div>
-      </div>
-      {/* end */}
-    </div>
+        {/* right */}
+        <div className="max-w-2xl flex flex-col gap-4">
+          <h1 className="text-3xl text-gray-700 font-bold">{title}</h1>
+          <h2 className="text-xl text-neutral-content font-bold mt-2">
+            {company}
+          </h2>
+          <h1 className="text-xl">${price / 100}</h1>
+          <p className="leading-8">{description}</p>
+          <div className="flex flex-col gap-4">
+            <h1 className="text-md font-medium tracking-wider capitalize">
+              Colors
+            </h1>
+            <div className="flex gap-3">
+              {colors.map((color, index) => {
+                return (
+                  <button
+                    key={color}
+                    type="button"
+                    className={`badge w-6 h-6 mr-2 ${
+                      color === productColor && "border-2 border-secondary"
+                    }`}
+                    style={{ backgroundColor: color }}
+                    onClick={() => setProductColor(color)}
+                  ></button>
+                );
+              })}
+            </div>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text text-2xl">Amount</span>
+              </div>
+              <select 
+              value={amount}
+              onChange={handleAmount}
+              className="select select-secondary select-md select-bordered w-full max-w-xs">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+              </select>
+            </label>
 
+            <div className="w-1/4">
+              <SubmitBtn text="ADD TO BAG" />
+            </div>
+          </div>
+        </div>
+        {/* end */}
+      </div>
     </div>
   );
 };
